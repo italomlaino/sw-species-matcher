2f55dcb623e496087caf017bdd439cf0dbc7f550
Ítalo Macedo Laino <italomlaino@gmail.com>
2019-10-27 21:50:42 -0300

    Add SpeciesMatcherControllerTest

---
+++ sw-species-matcher-api/src/test/java/com/italomlaino/swspeciesmatcher/api/controller/SpeciesMatcherControllerTest.java
@@ -0,0 +1,141 @@
+package com.italomlaino.swspeciesmatcher.api.controller;
+
+import com.italomlaino.swspeciesmatcher.api.exception.FailedToFetchCharacterException;
+import com.italomlaino.swspeciesmatcher.api.exception.FailedToFetchFilmException;
+import com.italomlaino.swspeciesmatcher.api.exception.FailedToFetchSpeciesException;
+import com.italomlaino.swspeciesmatcher.api.exception.SpeciesNotFoundException;
+import com.italomlaino.swspeciesmatcher.api.provider.swapico.SWApiClient;
+import com.italomlaino.swspeciesmatcher.api.service.URLService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.contains;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.doThrow;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class SpeciesMatcherControllerTest {
+
+    private static final String END_POINT = "/api/jdtest";
+    private static final String FILM_ID_PARAM = "film_id";
+    private static final String CHARACTER_ID_PARAM = "character_id";
+
+    @Autowired
+    private MockMvc mvc;
+    @SpyBean
+    private SWApiClient apiClient;
+    @SpyBean
+    private URLService urlService;
+
+    @Test
+    public void matches_happyDay() throws Exception {
+        mvc.perform(
+                getRequest("1", "2"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("[\"C-3PO\",\"R2-D2\",\"R5-D4\"]")));
+    }
+
+    @Test
+    public void matches_characterNotFound() throws Exception {
+        mvc.perform(
+                getRequest("1", "99999999999"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string(equalTo("{\"message\":\"Character not found\"}")));
+    }
+
+    @Test
+    public void matches_characterNotInTheFilm() throws Exception {
+        doReturn(-1L).when(urlService).getIdByURL(contains("people"));
+
+        mvc.perform(
+                getRequest("1", "2"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string(equalTo("{\"message\":\"Character is not in the film\"}")));
+    }
+
+    @Test
+    public void matches_speciesNotFound() throws Exception {
+        doThrow(new SpeciesNotFoundException()).when(apiClient).fetchSpecies(anyLong());
+
+        mvc.perform(
+                getRequest("1", "2"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string(equalTo("{\"message\":\"Species not found\"}")));
+    }
+
+    @Test
+    public void matches_filmNotFound() throws Exception {
+        mvc.perform(
+                getRequest("99999999999", "2"))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string(equalTo("{\"message\":\"Film not found\"}")));
+    }
+
+    @Test
+    public void matches_failedToFetchCharacter() throws Exception {
+        doThrow(new FailedToFetchCharacterException()).when(apiClient).fetchPeople(2);
+
+        mvc.perform(
+                getRequest("1", "2"))
+                .andExpect(status().isFailedDependency())
+                .andExpect(content().string(equalTo("{\"message\":\"Failed to fetch the character\"}")));
+    }
+
+    @Test
+    public void matches_failedToFetchFilm() throws Exception {
+        doThrow(new FailedToFetchFilmException()).when(apiClient).fetchFilm(1);
+
+        mvc.perform(
+                getRequest("1", "2"))
+                .andExpect(status().isFailedDependency())
+                .andExpect(content().string(equalTo("{\"message\":\"Failed to fetch the film\"}")));
+    }
+
+    @Test
+    public void matches_failedToFetchSpecies() throws Exception {
+        doThrow(new FailedToFetchSpeciesException()).when(apiClient).fetchSpecies(anyLong());
+
+        mvc.perform(
+                getRequest("1", "2"))
+                .andExpect(status().isFailedDependency())
+                .andExpect(content().string(equalTo("{\"message\":\"Failed to fetch the species\"}")));
+    }
+
+    @Test
+    public void matches_incorrectFilmIdParam() throws Exception {
+        mvc.perform(
+                getRequest("A", "2"))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string(equalTo("{\"message\":\"Failed to convert value of type 'java.lang.String' to required type 'long'; nested exception is java.lang.NumberFormatException: For input string: \\\"A\\\"\"}")));
+    }
+
+    @Test
+    public void matches_incorrectCharacterIdParam() throws Exception {
+        mvc.perform(
+                getRequest("1", "A"))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().string(equalTo("{\"message\":\"Failed to convert value of type 'java.lang.String' to required type 'long'; nested exception is java.lang.NumberFormatException: For input string: \\\"A\\\"\"}")));
+    }
+
+    private MockHttpServletRequestBuilder getRequest(String filmId, String characterId) {
+        return get(END_POINT)
+                .param(FILM_ID_PARAM, filmId)
+                .param(CHARACTER_ID_PARAM, characterId)
+                .accept(MediaType.APPLICATION_JSON);
+    }
+}
